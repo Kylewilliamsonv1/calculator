ba80126060e90aabcb9c91a8e1c0ceb49ff7910b
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-01 14:02:16 -0700

    add specific caculator functions to calculator machine

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+// alert(add(10,5));
+// let results=add(3,5,8,9);
+// alert(results)
+// const number1 = prompt("enter a number:");
+// alert(number1);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+// alert(add(number1,number2));
+
+function subtract(number1,number2) {
+  return number1 - number2;
+}
+// alert(subtract(number1,number2));
+
+function multiply(number1,number2) {
+  return number1 * number2;
+}
+// alert(multiply(number1,number2));
+
+function divide(number1,number2) {
+  return number1 / number2;
+}
+// alert(divide(number1,number2));
